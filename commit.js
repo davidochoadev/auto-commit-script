4a1c0958009900cc1f0026f124474e5a95587705
davideochoaa <davideochoaa@gmail.com>
2023-06-06 15:31:22 +0200

    🚀 First Commit

---
+++ commit.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+import chalk from "chalk";
+import simpleGit from "simple-git";
+
+const git = simpleGit();
+
+async function autoCommitFunc() {
+  const message =
+    process.argv.slice(3).join(" ") || "General Edits on Current Branch";
+  let messagePrefix;
+  let consoleMessage;
+  let isFirstCommit = false;
+
+  switch (process.argv.slice(2)[0]) {
+    case "fix":
+      messagePrefix = "🛠 Fix";
+      consoleMessage = "🛠 Fixing Commit Successful";
+      break;
+    case "feat":
+      messagePrefix = "✅ Feat";
+      consoleMessage = "✅ Featuring Commit Successful";
+      break;
+    case "refactoring":
+      messagePrefix = "✏️ Refactoring";
+      consoleMessage = "✏️ Refactoring Commit Successful";
+      break;
+    case "first":
+      messagePrefix = "🚀 First Commit";
+      consoleMessage = "🚀 First Commit Successful";
+      isFirstCommit = true;
+      await git.init(".");
+      await git.add(".");
+      await git.commit(`${messagePrefix}`);
+      await git.raw(["branch", "-m", "main"]);
+      await git.addRemote('origin',`${message}`);
+      await git.push(["-u", "origin", "main"]);
+      console.log(chalk.green(consoleMessage));
+      break;
+    default:
+      messagePrefix = "🤖 Automatic Commit";
+      consoleMessage = "🤖 Automaitc Commit Successful";
+  }
+
+  if( !isFirstCommit) {
+    await git.add(".");
+    await git.commit(`${messagePrefix}: ${message}`);
+    await git.push("origin", "main");
+    console.log(chalk.green(consoleMessage));
+  }
+}
+
+try {
+  autoCommitFunc().catch(err => console.log(chalk.red("Failed to Commit")));
+} catch (error) {
+  console.log(error);
+}
